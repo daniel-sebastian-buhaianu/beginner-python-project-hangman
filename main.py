01e6f4bdc791e9e8698c0bbade369f68a8b48fec
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-05 11:18:04 +0300

    same functionality, less code

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ URL = "https://www.randomlists.com/data/words.json"
 
 def main():
     words = load_words(URL)
-    random_index = get_random_index(words)
-    print(f"Random english word: {words[random_index]}")
+    print(f"Random english word: {random.choice(words)}")
 
 def load_words(url):
     json_url = urlopen(url)
     return json.loads(json_url.read())["data"]
-
-def get_random_index(list):
-    return random.randint(0, len(list) - 1)
-
 
 main()
