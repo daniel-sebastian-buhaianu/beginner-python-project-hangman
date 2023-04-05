0232704d02b55725ad12ee65ed7e99a11b694af1
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-05 12:04:15 +0300

    implemented hangman

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-import json
-import random
+import json, random, string
 from urllib.request import urlopen
+
 
 URL = "https://www.randomlists.com/data/words.json"
 
-def main():
-    words = load_words(URL)
-    print(f"Random english word: {random.choice(words)}")
+def get_random_word():
+    json_url = urlopen(URL)
+    words = json.loads(json_url.read())["data"]
+    return random.choice(words)
 
-def load_words(url):
-    json_url = urlopen(url)
-    return json.loads(json_url.read())["data"]
+def hangman():
+    word = get_random_word().upper()
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
 
-main()
+    # getting the user input
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(["a", "b", "cd"]) -> "a b cd"
+        print("You have used these letters: ", ' '.join(used_letters))
+
+        # what the current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+
+        print("Current word: ", ' '.join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        
+        elif user_letter in used_letters:
+            print("You have already used that letter. Please try again")
+
+        else:
+            print("Invalid. Please try again")
+
+    print(f"Yay, you guessed the word {word} !")
+
+
+# Run hangman
+hangman()
